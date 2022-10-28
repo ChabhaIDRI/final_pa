617c576dd6f0673a6eee293f950c6b4daf82b702
Chabha IDRI <48573108+ChabhaIDRI@users.noreply.github.com>
2022-10-28 09:00:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,27 +12,27 @@ stopwords=pickle.load(open('stopwords.pkl','rb'))
 
 #dd = pd.DataFrame(victor)
 
-def classify(document):
-    label = {0: 'negative', 1: 'positive'}
-    X = victor.transform([document])
+def classify(txt):
+    label = {1: 'negative', 0: 'positive'}
+    X = victor.transform([txt])
     y = model.predict(X)[0]
-    proba = np.max(model.predict_proba(X))
-    return label[y], proba
+    probabilite = np.max(model.predict_proba(X))
+    return label[y], probabilite
 
 
-class ReviewForm(Form):
-    moviereview = TextAreaField('',[validators.DataRequired(),validators.length(min=2)])
+class Tweet_Form(Form):
+    tweet_analyser = TextAreaField('',[validators.DataRequired(),validators.length(min=2)])
 
 @app.route('/')
 def index():
-    form = ReviewForm(request.form)
+    form = Tweet_Form(request.form)
     return render_template('1_ere_page.html', form=form)
 
 @app.route('/results', methods=['POST'])
 def results():
-    form = ReviewForm(request.form)
+    form = Tweet_Form(request.form)
     if request.method == 'POST' and form.validate():
-        tweet = request.form['moviereview']
+        tweet = request.form['tweet_analyser']
         y, proba = classify(tweet)
         return render_template('results.html',content=tweet,prediction=y,probability=round(proba*100, 2))
     return render_template('1_ere_page.html', form=form)
@@ -40,28 +40,3 @@ def results():
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-"""""
-@app.route('/')
-def hello_world():
-    return render_template("analyse_sentiment.html")
-
-
-@app.route('/predict',methods=['POST','GET'])
-def predict():
-    label = {0: 'negative', 1: 'positive'}
-    entrée=[str(x) for x in request.form.values()]
-    entrée_array=[np.array(entrée)]
-    prediction=model.predict(entrée_array)
-
-   
-    if output>str(0.5):
-        return render_template('analyse_sentiment.html',pred='le tweet est positif {}'.format(output))
-        print ('hello')
-    else:
-        return render_template('analyse_sentiment.html',pred='le tweet est négatif {}'.format(output))
-        print ('hello2')
-
-if __name__ == '__main__':
-    app.run()
-"""
